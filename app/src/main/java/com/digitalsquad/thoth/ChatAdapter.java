52b083c4663f90292ff56bef014a8813f06aef56
Ronak Patel <ronakp991@gmail.com>
2016-09-19 14:20:38 -0400

    Format Ready, Message Remaining

--- app/src/main/java/com/digitalsquad/thoth/ChatAdapter.java
+++ app/src/main/java/com/digitalsquad/thoth/ChatAdapter.java
@@ -74,21 +74,6 @@ public class ChatAdapter extends BaseAdapter {
 
     private void setAlignment(ViewHolder holder, boolean isMe) {
         if (!isMe) {
-            holder.contentWithBG.setBackgroundResource(R.drawable.in_message_bg);
-
-            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) holder.contentWithBG.getLayoutParams();
-            layoutParams.gravity = Gravity.RIGHT;
-            holder.contentWithBG.setLayoutParams(layoutParams);
-
-            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) holder.content.getLayoutParams();
-            lp.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0);
-            lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            holder.content.setLayoutParams(lp);
-            layoutParams = (LinearLayout.LayoutParams) holder.txtMessage.getLayoutParams();
-            layoutParams.gravity = Gravity.RIGHT;
-            holder.txtMessage.setLayoutParams(layoutParams);
-            layoutParams.gravity = Gravity.RIGHT;
-        } else {
             holder.contentWithBG.setBackgroundResource(R.drawable.out_message_bg);
 
             LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) holder.contentWithBG.getLayoutParams();
@@ -103,6 +88,21 @@ public class ChatAdapter extends BaseAdapter {
             layoutParams.gravity = Gravity.LEFT;
             holder.txtMessage.setLayoutParams(layoutParams);
             layoutParams.gravity = Gravity.LEFT;
+        } else {
+            holder.contentWithBG.setBackgroundResource(R.drawable.in_message_bg);
+
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) holder.contentWithBG.getLayoutParams();
+            layoutParams.gravity = Gravity.RIGHT;
+            holder.contentWithBG.setLayoutParams(layoutParams);
+
+            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) holder.content.getLayoutParams();
+            lp.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0);
+            lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            holder.content.setLayoutParams(lp);
+            layoutParams = (LinearLayout.LayoutParams) holder.txtMessage.getLayoutParams();
+            layoutParams.gravity = Gravity.RIGHT;
+            holder.txtMessage.setLayoutParams(layoutParams);
+            layoutParams.gravity = Gravity.RIGHT;
         }
     }
 
--- app/src/main/java/com/digitalsquad/thoth/Home.java
+++ app/src/main/java/com/digitalsquad/thoth/Home.java
@@ -3,6 +3,7 @@ package com.digitalsquad.thoth;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -23,15 +25,12 @@ public class Home extends Activity {
     private ImageButton sendBtn;
     private ChatAdapter adapter;
     private ArrayList<MessageData> chatHistory;
-
+    String phoneNo = "5199967698"; //2267740474
+    String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        initControls();
-    }
-
-    private void initControls() {
         messagesContainer = (ListView) findViewById(R.id.mainlist);
         messageET = (EditText) findViewById(R.id.chat);
         sendBtn = (ImageButton) findViewById(R.id.send);
@@ -40,22 +39,29 @@ public class Home extends Activity {
         RelativeLayout container = (RelativeLayout) findViewById(R.id.contain);
         loadDummyHistory();
 
-        sendBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String messageText = messageET.getText().toString();
-                if (TextUtils.isEmpty(messageText)) {
-                    return;
-                }
+    }
 
-                MessageData chatMessage = new MessageData();
-                chatMessage.setId(122);//dummy
-                chatMessage.setMessage(messageText);
-                chatMessage.setMe(true);
-                messageET.setText("");
-                displayMessage(chatMessage);
-            }
-        });
+    public void sendonclick(View v){
+        String messageText = messageET.getText().toString();
+        /*if (TextUtils.isEmpty(messageText)) {
+            return;
+        }*/
+        MessageData chatMessage = new MessageData();
+        chatMessage.setId(122);//dummy
+        chatMessage.setMessage(messageText);
+        chatMessage.setMe(true);
+        messageET.setText("");
+        displayMessage(chatMessage);
+        message = chatMessage.getMessage();
+        Toast.makeText(getApplicationContext(),"check",Toast.LENGTH_LONG).show();
+        try{
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(phoneNo, null, "hello", null, null);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(getApplicationContext(),"Cannot send",Toast.LENGTH_LONG).show();
+        }
     }
 
     public void displayMessage(MessageData message) {
@@ -72,7 +78,7 @@ public class Home extends Activity {
 
         chatHistory = new ArrayList<MessageData>();
 
-        MessageData msg = new MessageData();
+        /*MessageData msg = new MessageData();
         msg.setId(2);
         msg.setMe(false);
         msg.setMessage("Hi");
@@ -82,7 +88,7 @@ public class Home extends Activity {
         msg1.setId(2);
         msg1.setMe(false);
         msg1.setMessage("How r u doing???");
-        chatHistory.add(msg1);
+        chatHistory.add(msg1);*/
 
         adapter = new ChatAdapter(Home.this, new ArrayList<MessageData>());
         messagesContainer.setAdapter(adapter);
